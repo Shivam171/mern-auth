63a761a33dc1bb81c8800442171afd9e81007a7f
Shivam171 <shiv.op@gmail.com>
2024-10-13 13:20:38 +0530

    Server Routes and Database Initial Setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Connect to DB and start server
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+})
