d2bf244cb39cc918f788e1f65cfbcdb5cfed8ed6
Shivam171 <shiv.op@gmail.com>
2024-10-14 22:27:58 +0530

    Client Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import FloatingShape from "./components/FloatingShape"
+import { Routes, Route } from "react-router-dom"
+import SignUpPage from "./pages/SignUpPage"
+import LogInPage from "./pages/LogInPage"
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex justify-center items-center relative overflow-hidden">
+      <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
+      <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
+      <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/forgot-password" element={"Forgot Password"} />
+      </Routes>
+    </div>
+  )
+}
