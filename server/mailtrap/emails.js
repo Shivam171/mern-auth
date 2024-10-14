df486662dfea73e797ba192a581dcc90c182d3b6
Shivam171 <shiv.op@gmail.com>
2024-10-14 20:15:48 +0530

    Verifying Email Endpoint and Building a welcome email template

--- server/mailtrap/emails.js
+++ server/mailtrap/emails.js
@@ -11,10 +11,28 @@ export const sendVerificationEmail = async (email, verificationToken) => {
             html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
             category: "Email Verification"
         })
-        console.log("Email send successfully", response);
-
+        // console.log("Verification email send successfully", response);
     } catch (error) {
         console.log("Error sending verification email", error);
         throw new Error(`Error sending verification email: ${error}`);
+    }
+}
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "ada70845-2f76-46df-9ba3-c57c79c74c3c",
+            template_variables: {
+                "company_info_name": "MERN AUTH Tutorial",
+                "name": name
+            }
+        })
+        console.log("Welcome email send successfully", response);
+    } catch (error) {
+        console.log("Error sending welcome email", error);
+        throw new Error(`Error sending welcome email: ${error}`);
     }
 }
